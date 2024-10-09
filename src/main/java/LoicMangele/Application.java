56ca8d86381e8ec18115faca51034165b111b5a5
sudonona <80674485+sudonona@users.noreply.github.com>
2024-10-09 18:05:14 +0200

    adds

--- src/main/java/LoicMangele/Application.java
+++ src/main/java/LoicMangele/Application.java
@@ -8,7 +8,6 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("E3W3");
@@ -17,9 +16,10 @@ public class Application {
         EntityManager em = emf.createEntityManager();
         EventsDao ev = new EventsDao(em);
         PersonDao pe = new PersonDao(em);
+
+
         Location location1 = new Location("Concert Hall", "New York");
         Location location2 = new Location("San Siro", "Milano");
-
 
         em.getTransaction().begin();
         em.persist(location1);
@@ -34,19 +34,20 @@ public class Application {
                 TipoEvento.Privato,
                 500, location1
         );
+
         Evento topOfThePops = new Evento(
                 "Top of the Pops",
                 LocalDate.of(2025, 07, 13),
                 "L'evento musicale estivo è tornato",
                 TipoEvento.Publico,
                 7000, location2
-
         );
 
 
+        em.getTransaction().begin();
         ev.save(beatles);
         ev.save(topOfThePops);
-
+        em.getTransaction().commit();
 
 
         Persona person = new Persona(
@@ -57,7 +58,18 @@ public class Application {
                 Sesso.MASCHIO
         );
 
+
+        em.getTransaction().begin();
         pe.save(person);
+        em.getTransaction().commit();
+
+
+        Partecipazioni partecipazione = new Partecipazioni(person, beatles, StatoPartecipazione.CONFERMATO);
+
+
+        em.getTransaction().begin();
+        em.persist(partecipazione);
+        em.getTransaction().commit();
 
         em.close();
         emf.close();
